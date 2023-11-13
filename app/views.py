b98ae2d11825850d5157b691f88febc8cbdc7f2e
sekhar6531 <kulasekhar6531@gmail.com>
2023-11-13 11:18:54 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sekhar(request):
+    return render(request,'sekhar.html')
+def reddy(request):
+    return render(request,'reddy.html')
+def ntr(request):
+    return render(request,'ntr.html')
+def devara(request):
+    return render(request,'devara.html')
